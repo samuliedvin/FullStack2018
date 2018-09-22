2b86f9d292ae57853f97b33b87941950dc1f9db0
Samuli Virtanen <samuliedvin@gmail.com>
2018-09-22 12:03:05 +0300

    add exercise 1.2. completed

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -27,9 +27,15 @@ const Otsikko = (props) => (
 
 const Sisalto = (props) => (
     <div>
-        <p>{props.osa1} {props.tehtavia1}</p>
-        <p>{props.osa2} {props.tehtavia2}</p>
-        <p>{props.osa3} {props.tehtavia3}</p>
+        <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+        <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+        <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
+    </div>
+)
+
+const Osa = (props) => (
+    <div>
+        <p>{props.osa} {props.tehtavia}</p>
     </div>
 )
 
